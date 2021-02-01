4a3a2146596f996a850bf512e7e84c3bd7ec579f
Marius Tita <marius.tita81@gmail.com>
2021-02-01 10:58:17 +0200

    update litigii inghetate - select_date

--- R/mod_litigii_inghetate.R
+++ R/mod_litigii_inghetate.R
@@ -10,6 +10,9 @@
 mod_litigii_inghetate_ui <- function(id){
   ns <- NS(id)
   tagList(
+      selectInput(inputId = ns("select_date"),label = "Selecteaza data soldului inghetat",
+                  choices = c(as.Date("2020-12-31"),as.Date("2020-09-30"))),
+      
       DT::dataTableOutput(ns("sinteza_sold_litigii")), hr(),
       
       downloadButton(ns("down_litigii_inghetate"),label = "Download litigii inghetate"),br(),
@@ -23,24 +26,28 @@ mod_litigii_inghetate_ui <- function(id){
 #' @noRd 
 mod_litigii_inghetate_server <- function(input, output, session){
   ns <- session$ns
-  load('data/litigii_sep.rda')
+  load('data/litigii_inghetate.rda')
   
   output$sinteza_sold_litigii <- DT::renderDataTable({ DT::datatable(class = "compact",
-                  caption = "Sinteza sold litigii 30 septembrie 2020:",extensions = "Buttons",
+                  caption = paste0("Sinteza sold litigii la ",input$select_date),
+                  extensions = "Buttons",
                   options = list(dom = "Bt", buttons = c("excel", "csv")),  rownames = FALSE,
-    data = litigii_sep %>% dplyr::group_by(Tip_litigiu) %>% dplyr::summarise(Suma_litigiu = sum(`Suma litigiu - echiv lei`),
-            Provizion_septembrie = sum(`Necesar provizion septembrie 2020`),
+    data = litigii_inghetate %>% dplyr::filter(data_raport == input$select_date) %>% 
+            dplyr::group_by(Tip_litigiu) %>% dplyr::summarise(Suma_litigiu = sum(`Suma litigiu - echiv lei`),
+            Necesar_provizion = sum(`Necesar_provizion`),
             Numar_litigii = dplyr::n()) %>% dplyr::arrange(desc(Suma_litigiu)) %>% 
             janitor::adorn_totals()) %>%   
             DT::formatRound(columns = 2:4, digits = 0)  })
   
   output$sold_litigii_inghetat <- DT::renderDataTable({ DT::datatable(class = "nowrap",
-     caption = "Sold detaliat la 30 septembrie 2020:",
+     caption = paste0("Sold detaliat la ",input$select_date),
      options = list(dom = "ftp", scrollX = TRUE, pageLength = 7),  rownames = FALSE,
       data = litigii_sep %>% dplyr::select(-Nr_crt))  })
   
-  output$down_litigii_inghetate <- downloadHandler(filename = function() {"litigii_inghetate.csv"},
-      content = function(file) {readr::write_csv(x = litigii_sep, path = file)})
+  output$down_litigii_inghetate <- downloadHandler(filename = function() {
+    paste0("litigii_inghetate",input$select_date,".csv")},
+      content = function(file) {readr::write_csv(x = litigii_inghetate %>% 
+                                    dplyr::filter(data_raport == input$select_date), path = file)})
   
 }
     
